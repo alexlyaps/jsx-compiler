5717267ea9e6e1bd4b8d850cb2e242121e9576e0
alexlyaps <a34270622@gmail.com>
2026-05-14 23:27:32 +0200

    add clumsy reactivity

--- src/main.js
+++ src/main.js
@@ -1,11 +1,29 @@
 import { h } from "./compiler/h";
 import { compileJSXtoJS } from "./compiler/compiler";
+import { Observer } from "./state/observer";
 
 const root = document.querySelector("#app");
-const jsx = "<h1>Hello You</h1>";
-const code = compileJSXtoJS(jsx);
 
-const fn = new Function("h", `return ${code}`);
-const result = fn(h);
+const observer = new Observer({ name: "Alex" });
+observer.subscribe(render);
 
-root.appendChild(result);
+const input = document.createElement("input");
+
+input.addEventListener("input", (e) => {
+  const value = e.target.value;
+  console.log(value);
+  observer.setState({ name: value });
+});
+
+function render() {
+  const jsx = `<h1>Hello ${observer.getState().name}</h1>`;
+  const code = compileJSXtoJS(jsx);
+  const fn = new Function("h", `return ${code}`);
+  const result = fn(h);
+
+  root.innerHTML = "";
+  root.appendChild(result);
+  root.appendChild(input);
+}
+
+render();
---
+++ src/state/observer.js
@@ -0,0 +1,28 @@
+export class Observer {
+  constructor(initialValue) {
+    this.state = initialValue;
+    this.listeners = [];
+  }
+
+  subscribe(listener) {
+    this.listeners.push(listener);
+
+    return () => this.unsubscribe(listener);
+  }
+
+  unsubscribe(listener) {
+    this.listeners = this.listeners.filter((l) => l !== listener);
+  }
+
+  setState(nextState) {
+    if (this.state === nextState) return;
+    this.state = nextState;
+    this.listeners.forEach((l) => l());
+  }
+
+  getState() {
+    return this.state;
+  }
+}
+
+// so far, listener is a function.. from state?
